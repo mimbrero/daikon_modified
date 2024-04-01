cc01381f7a06324e82b3f02d256eac2a38528a0b
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-04-01 17:21:48 +0200

    SequenceStringElementsAreDateMMDDYYYY Postman assertion

--- java/daikon/inv/unary/stringsequence/dates/SequenceStringElementsAreDateMMDDYYYY.java
+++ java/daikon/inv/unary/stringsequence/dates/SequenceStringElementsAreDateMMDDYYYY.java
@@ -16,6 +16,8 @@ import typequals.prototype.qual.Prototype;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static daikon.agora.PostmanUtils.getPostmanVariableName;
+
 /**
  * Represents string sequences that contain a common subset. Prints as {@code {s1, s2, s3, ...}
  * subset of x[]}.
@@ -32,6 +34,8 @@ public class SequenceStringElementsAreDateMMDDYYYY extends SingleStringSequence 
 
   // Set to true if the array is empty. If we do not use this property, the invariant would be considered true if all the arrays are empty
   private boolean alwaysEmpty = true;
+
+  private static String regex = "^(?:0[1-9]|1[012])[-/.](?:0[1-9]|[12][0-9]|3[01])[-/.](?:19\\d{2}|20[0134][0-9]|2050)$";
 
   protected SequenceStringElementsAreDateMMDDYYYY(PptSlice ppt) {
     super(ppt);
@@ -79,7 +83,7 @@ public class SequenceStringElementsAreDateMMDDYYYY extends SingleStringSequence 
     }
 
     if (format == OutputFormat.POSTMAN) {
-      return "TODO: IMPLEMENT POSTMAN ASSERTION";
+      return "pm.expect(" + getPostmanVariableName(var().name()) + ".every(element => /" + regex + "/.test(element))).to.be.true";
     }
 
     return format_unimplemented(format);
@@ -98,7 +102,7 @@ public class SequenceStringElementsAreDateMMDDYYYY extends SingleStringSequence 
      *       - 10-30-2050
      */
     // ^(?:0[1-9]|1[012])[-/.](?:0[1-9]|[12][0-9]|3[01])[-/.](?:19\d{2}|20[0134][0-9]|2050)$
-    Pattern pattern = Pattern.compile("^(?:0[1-9]|1[012])[-/.](?:0[1-9]|[12][0-9]|3[01])[-/.](?:19\\d{2}|20[0134][0-9]|2050)$");
+    Pattern pattern = Pattern.compile(regex);
 
     if(a.length>0){
       alwaysEmpty = false;
