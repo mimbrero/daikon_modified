e1796f34e5038421cc8d42c208a9443abc6b4143
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-04-01 09:43:38 +0200

    Postman assertion MemberString

--- java/daikon/inv/binary/sequenceString/MemberString.java
+++ java/daikon/inv/binary/sequenceString/MemberString.java
@@ -23,6 +23,7 @@ import typequals.prototype.qual.NonPrototype;
 import typequals.prototype.qual.Prototype;
 
 import static daikon.Daikon.use_modified_daikon_version;
+import static daikon.agora.PostmanUtils.getPostmanVariableName;
 
 /**
  * Represents String scalars that are always members of a sequence of String values. Prints as
@@ -227,6 +228,8 @@ public final class MemberString extends SequenceString {
       return format_esc();
     } else if (format == OutputFormat.CSHARPCONTRACT) {
       return format_csharp_contract();
+    } else if (format == OutputFormat.POSTMAN) {
+      return format_postman();
     } else {
       return format_unimplemented(format);
     }
@@ -236,6 +239,10 @@ public final class MemberString extends SequenceString {
     String sclname = sclvar().name();
     String seqname = seqvar().name();
     return sclname + " in " + seqname;
+  }
+
+  public String format_postman(@GuardSatisfied MemberString this) {
+    return "pm.expect(" + getPostmanVariableName(seqvar().name()) + ".includes(" + getPostmanVariableName(sclvar().name()) + ")).to.be.true";
   }
 
   public String format_java() {
