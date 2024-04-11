b2cb922528d562ff14249322152f31e74234c48c
Juan Carlos Alonso Valenzuela <44436290+JuanCarlosAlonsoValenzuela@users.noreply.github.com>
2024-04-11 10:28:36 +0200

    Update PostmanUtils.java

--- java/daikon/agora/PostmanUtils.java
+++ java/daikon/agora/PostmanUtils.java
@@ -8,13 +8,10 @@ public class PostmanUtils {
 
     // TODO: THIS METHOD MUST BE IDENTICAL TO THE ONE IN DAIKON, every modification performed here must be performed in Daikon too!!!
     // TODO: Document properly (with multiple input/output example)
-    // TODO: Consider moving to a different class
     // TODO: Program points that are nested arrays? (e.g., GitHub)
-    // TODO: Replace special characters (e.g., kebab case is not allowed in JS)
     // Returns the variable name in the format used in the Postman assertion
     public static String getPostmanVariableName(String originalVariableName) {
 
-        // TODO: Test this if clause
         if(!originalVariableName.startsWith("input.") &&
                 !originalVariableName.startsWith("return.") &&
                 !originalVariableName.startsWith("size(input.") &&
@@ -27,12 +24,12 @@ public class PostmanUtils {
 
         // If the variable contains shift
         String shiftSuffix = "";
-        if (postmanVariableName.matches(".* [+]{1}[0-9]{1,}$")) {            // increase
+        if (postmanVariableName.matches(".* [+][0-9]+$")) {            // increase
             int plusIndex = postmanVariableName.lastIndexOf(" +");
             shiftSuffix = "_plus_" + postmanVariableName.substring(plusIndex+2);
             postmanVariableName = postmanVariableName.substring(0, plusIndex);
 
-        } else if (postmanVariableName.matches(".* [-]{1}[0-9]{1,}$")) {    // decrease
+        } else if (postmanVariableName.matches(".* -[0-9]+$")) {    // decrease
             int minusIndex = postmanVariableName.lastIndexOf(" -");
             shiftSuffix = "_minus_" + postmanVariableName.substring(minusIndex+2);
             postmanVariableName = postmanVariableName.substring(0, minusIndex);
